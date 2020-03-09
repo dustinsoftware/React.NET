28160c8a52908387e3ba3e51901766ab0ddaafd8
Dustin Masters <ceo@dustinsoftware.com>
2020-03-08 22:05:29 -0700

    Webpack template: Spawn webpack dev server in background

--- src/React.Template/reactnet-webpack/Program.cs
+++ src/React.Template/reactnet-webpack/Program.cs
@@ -1,5 +1,11 @@
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace React.Sample.Webpack.CoreMvc
 {
@@ -13,6 +19,32 @@ namespace React.Sample.Webpack.CoreMvc
 		public static IWebHost BuildWebHost(string[] args) =>
 			WebHost.CreateDefaultBuilder(args)
 				.UseStartup<Startup>()
+				.ConfigureServices((hostContext, service) =>
+				{
+					service.AddHostedService<WebpackDevServer>();
+				})
 				.Build();
+
+		private class WebpackDevServer : BackgroundService
+		{
+			protected override Task ExecuteAsync(CancellationToken token)
+			{
+				if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+				{
+					var devServer = Process.Start(new ProcessStartInfo
+					{
+						Arguments = "/c npm i && npm run build -- --watch",
+						RedirectStandardError = false,
+						RedirectStandardInput = false,
+						RedirectStandardOutput = false,
+						FileName = "cmd.exe"
+					});
+
+					token.Register(() => devServer?.Kill(true));
+				}
+
+				return Task.Delay(Timeout.Infinite, token);
+			}
+		}
 	}
 }
